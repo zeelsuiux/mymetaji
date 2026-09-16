779797824cb57d6cf02f6390629c8f953f7085da
zeelsdeveloper-oss <zeels.developer@gmail.com>
2026-09-16 23:59:08 +0530

    your message 1

--- team-details.php
+++ team-details.php
@@ -225,3 +225,5 @@ require __DIR__ . '/includes/header.php';
                         </div>
                     </div>
                 </section>
+
+<?php require __DIR__ . '/includes/footer.php'; ?>
